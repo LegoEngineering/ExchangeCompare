056e141e550ec0649ef97cd30ce22719e89262a5
LegoEngineering <brick.andrew93@gmail.com>
2018-01-02 11:04:42 -0600

    web app now reporting data accurately and updating every 10 seconds

--- react_client/src/App.js
+++ react_client/src/App.js
@@ -8,15 +8,16 @@ class App extends Component {
         super(props);
         this.state = {
             data:[],
-            count:0
+            count: 0
         };
-        this.interval = setInterval(this.updateDate, 5000);
+        this.interval = setInterval(this.updateDate, 10000);
 
     }
     componentDidMount() {
         fetch('/kraken')
             .then(res => res.json())
-            .then(data => this.setState({ data }));
+            .then(data => this.setState({data: data, count: this.state.count} ))
+
     }
 
     componentWillUnmount() {
@@ -25,21 +26,28 @@ class App extends Component {
     updateDate= () => {
         fetch('/kraken')
             .then(res => res.json())
-            .then(data => this.setState({ data }));
+            .then(data => this.setState({data: data, count: this.state.count+1} ))
     }
 
     render() {
+        const num = this.state.count;
         return (
             <div className="App">
                 <h1>Exchange Data</h1>
+                <h1> {this.state.count} </h1>
                 <h4>Dash Price       ETH Price       LTC Price</h4>
-                {this.state.data.filter(({Index}) => Index === 5).map(data =>
-                    <div key={data.Index}>{data.Exchange}: {data.DASH_price} {data.ETH_price} {data.LTC_price}</div>
+                {this.state.data.filter(({Index}) => Index === num).map(data =>
+                    <div>{data.Exchange}:
+                        <ul>DASH: {data.DASH_price}</ul>
+                        <ul>ETH:  {data.ETH_price}</ul>
+                        <ul>LTC:  {data.LTC_price}</ul></div>
                 )}
+
             </div>
         );
     }
 }
-
+//{this.state.data.filter(({Index}) => Index === 5).map(data =>
+  //  <div key={data.Index}>{data.Exchange}: {data.DASH_price} {data.ETH_price} {data.LTC_price}</div>
 
 export default App;
